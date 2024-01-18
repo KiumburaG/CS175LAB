84cd6579ae84cd0e4f4baafef2a15a5b11b8991d
KiumburaG <146123137+KiumburaG@users.noreply.github.com>
2024-01-18 12:58:18 -0500

    Add files via upload

---
+++ myInfo.py
@@ -0,0 +1,7 @@
+#CS175L
+#Your first name and Last name
+#myInfo
+
+print('My name is: Kiumbura Githinji')
+print('My address is: 45 Merrit Ave, Bergenfield, NJ 07621')
+print('My phone number is: 123-456-9877')
